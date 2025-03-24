7ce4111a51e0013fa7212fdc03ebb116636d5f6a
Sandr0ta <144434115+Sandr0ta@users.noreply.github.com>
2025-03-24 22:14:35 +0000

    Update Program.cs

--- FixSubroutineTask/Program.cs
+++ FixSubroutineTask/Program.cs
@@ -4,14 +4,21 @@
     {
         static void Main(string[] args)
         {
-            for(int i = 8;i >= 0; i++) {
+            for(int i = 7 ;i >= 0; i--) {
                 Console.WriteLine(diffCurrencies(i));            
             }
         }
         static string diffCurrencies(int x)
         {
             string[] currencies = { "baht", "dollar", "euro", "koruna", "lira", "rand", "rupee", "yen" };
-            return currencies[1];
+            if (x >= 0 && x < currencies.Length)
+            {
+                return currencies[1];
+            }
+            else
+            {
+                return "Invalid";
+            }
 
         }
     }
